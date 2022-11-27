8b1b3d0fdfac4b46af20794089bed75aa1f5355a
Pvandw33ch <112890171+Pvandw33ch@users.noreply.github.com>
2022-11-27 18:35:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const imagePath     = "images"
+const imageArray    = [
+    "Anya\ Forger.jpg",
+    "Anya\ Forger\ \(1\).jpg",
+    "Anya\ Forger\ \(5\).jpg",
+    "Anya\ Forger\ \(6\).jpg",
+    "Anya\ Forger\ \(7\).jpg",
+]
+let counter = 1
+
+document.getElementById("myButton").onclick = function(){
+    document.body.style.backgroundImage = `url('${imagePath}/${imageArray[counter]}')`
+    counter = counter == (imageArray.length - 1)? 0 : (counter + 1)
+}
